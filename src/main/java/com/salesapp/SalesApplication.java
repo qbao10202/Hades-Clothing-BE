96fff7fa0eba71a1a680b86af41edfe8d3b19809
qbao10202 <quocbao01652@gmail.com>
2025-07-17 23:36:47 +0700

    update

--- src/main/java/com/salesapp/SalesApplication.java
+++ src/main/java/com/salesapp/SalesApplication.java
@@ -1,5 +1,6 @@
 package com.salesapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
@@ -18,6 +19,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SalesApplication {
 
     public static void main(String[] args) {
+
+        Dotenv  dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // Set manually if needed
+        System.setProperty("MYSQL_DATABASE", dotenv.get("MYSQL_DATABASE"));
+        System.setProperty("MYSQL_PUBLIC_URL", dotenv.get("MYSQL_PUBLIC_URL"));
+        System.setProperty("MYSQL_ROOT_PASSWORD", dotenv.get("MYSQL_ROOT_PASSWORD"));
+        System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
+        System.setProperty("MYSQLDATABASE", dotenv.get("MYSQLDATABASE"));
+        System.setProperty("MYSQLHOST", dotenv.get("MYSQLHOST"));
+        System.setProperty("MYSQLPASSWORD", dotenv.get("MYSQLPASSWORD"));
+        System.setProperty("MYSQLPORT", dotenv.get("MYSQLPORT"));
+        System.setProperty("MYSQLUSER", dotenv.get("MYSQLUSER"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
         SpringApplication.run(SalesApplication.class, args);
     }
 } 
